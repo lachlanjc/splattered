2d6885768dbaecfe368bcb1a92ba6651c439769d
Lachlan Campbell <lachlan@hackclub.com>
2020-05-15 23:57:34 +0000

    Convert to serverless

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 Microservice for making rad patterns. (The name is a partial anagram.)
 
-Built with [micro](https://github.com/zeit/micro) & [GeoPattern](https://github.com/btmills/geopattern).
+Built with [GeoPattern](https://github.com/btmills/geopattern).
 
-MIT licensed.
+MIT License
 
 ---
-*Another thing by [@lachlanjc](https://twitter.com/lachlanjc).*
+
+*Another thing by [@lachlanjc](https://lachlanjc.com).*
--- api/[slug].js
+++ api/[slug].js
@@ -1,14 +1,10 @@
-const { send } = require('micro')
-const { router, get } = require('microrouter')
 const GeoPattern = require('geopattern')
 
 const pattern = (text = 'Noodles 2') =>
   GeoPattern.generate(text, { baseColor: '#ff6d00' })
 
-const data = (req, res) => {
+export default (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Content-Type', 'image/svg+xml')
-  send(res, 200, pattern(req.params.text).toString())
+  res.send(pattern(req.query.slug).toString())
 }
-
-module.exports = router(get('/', data), get('/:text', data))
